3147acff0dd699f7a250f0bea049a9899a7ba70c
Mikhail Eshchenko <mikhail.eshchenko@gmail.com>
2024-01-30 14:50:46 +0800

    change rbga color to fixed 255 with opacity

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,39 +1,42 @@
 export function drawCRTPixel (ctx:CanvasRenderingContext2D,colorsArr: Uint8ClampedArray, xCoord: number, yCoord: number, pixelWidth:number, pixelHeight:number){
-    const colorWidth = pixelWidth / 100 * 30;
-    const spaceWidth = pixelWidth / 100 * 5;
+    const colorWidth = pixelWidth / 100 * 32;
+    const spaceWidth = pixelWidth / 100 * 3;
     let curX = xCoord;
     let curY = yCoord;
 
+    // а что если делать не тусклость, а прозрачность?
+    // 255 - это 100%, 0 - это 0%
+    // 25 - это 10%
+    // 25.5/255*100 = 10
+    // Прозрачность будет нужна 0.1 -> значит на 100 умножать не надо
+
     ctx.beginPath();
     ctx.roundRect(curX, curY, colorWidth,pixelHeight, 5 )
-    ctx.fillStyle = `rgba(${String(colorsArr[0])},0,0,1)`;
+    ctx.fillStyle = `rgba(255,0,0,${colorsArr[0]/255})`;
     curX = curX + colorWidth + spaceWidth;
     ctx.fill();
 
-
     ctx.beginPath();
-    ctx.fillStyle = `rgba(0,${String(colorsArr[1])},0,1)`;
+    ctx.fillStyle = `rgba(0,255,0,${colorsArr[1]/255})`;
     ctx.roundRect(curX, curY, colorWidth,pixelHeight, 5 )
     curX = curX + colorWidth + spaceWidth;
     ctx.fill();
 
     ctx.beginPath();
-    ctx.fillStyle = `rgba(0,0,${String(colorsArr[1])},1)`;
+    ctx.fillStyle = `rgba(0,0,255,${colorsArr[2]/255})`;
     ctx.roundRect(curX, curY, colorWidth,pixelHeight, 5 )
     ctx.fill();
-
-    console.log('width, height: ', colorWidth*3 + spaceWidth * 2, pixelHeight);
 }
 
 export function drawPixels(ctx:CanvasRenderingContext2D,startX:number, startY:number, width:number, height:number, pixelsQuantity:number, colorsArr:Uint8ClampedArray){
     const clusterWidth = width / pixelsQuantity;
     const clusterHeight = height / pixelsQuantity;
 
-    const pixelWidth = clusterWidth / 100 * 95;
-    const spaceWidth = clusterWidth / 100 * 5;
+    const pixelWidth = clusterWidth / 100 * 98;
+    const spaceWidth = clusterWidth / 100 * 2;
 
-    const pixelHeight = clusterHeight / 100 * 95;
-    const spaceHeight = clusterHeight / 100 * 5;
+    const pixelHeight = clusterHeight / 100 * 98;
+    const spaceHeight = clusterHeight / 100 * 2;
 
     let curX = startX;
     let curY = startY;
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import {drawPixels, drawCRTPixel} from './helpers'
 const canvas = document.getElementsByTagName("canvas")[0];
 const ctx = canvas.getContext('2d');
 
-const pixelColors = ['#0e367c','#fba257','#ab3434'];
+const pixelColors = ['#47bffe','#f0cfcf','#88ff1f','#93d4a5'];
 
-ctx.fillStyle = '#9aa296';
+ctx.fillStyle = '#4f534d';
 ctx.fillRect(0,0,canvas.width, canvas.height);
 ctx.fill();
 
@@ -27,33 +27,76 @@ ctx.fillStyle = pixelColors[2];
 ctx.fill();
 ctx.closePath();
 
+ctx.beginPath();
+ctx.roundRect(0, 330, 100,100, 0 )
+ctx.fillStyle = pixelColors[3];
+ctx.fill();
+ctx.closePath();
+
 const img1 = new Image();
 img1.src = 'download.png';
 img1.onload = function (){
-    ctx.drawImage(img1, 0,0, 100,100)
-}
+    // ctx.drawImage(img1, 0,0, 100,100)
 
 let pixelColorArr1 = ctx.getImageData(10,10,1,1).data;
-let pixelColorArr2 = ctx.getImageData(0,120,1,1).data;
-let pixelColorArr3 = ctx.getImageData(0,220,1,1).data;
+let pixelColorArr2 = ctx.getImageData(10,110,1,1).data;
+let pixelColorArr3 = ctx.getImageData(10,220,1,1).data;
+let pixelColorArr4= ctx.getImageData(10,330,1,1).data;
 
+const pixelQnty = 20;
 
+drawPixels(ctx,200,0, 100, 100, pixelQnty, pixelColorArr1);
+drawPixels(ctx,200,110, 100, 100, pixelQnty, pixelColorArr2);
+drawPixels(ctx,200,220, 100, 100, pixelQnty, pixelColorArr3);
+drawPixels(ctx,200,330, 100, 100, pixelQnty, pixelColorArr4);
 
-const pixelQnty = 4;
+// drawPixels(ctx,200,31, 60, 60, pixelQnty, pixelColorArr1);
+// drawPixels(ctx,231,31, 60, 60, pixelQnty, pixelColorArr2);
+// drawPixels(ctx,262,31, 60, 60, pixelQnty, pixelColorArr3);
+// drawPixels(ctx,293,31, 60, 60, pixelQnty, pixelColorArr4);
+//
+// drawPixels(ctx,200,62, 60, 60, pixelQnty, pixelColorArr1);
+// drawPixels(ctx,231,62, 60, 60, pixelQnty, pixelColorArr2);
+// drawPixels(ctx,262,62, 60, 60, pixelQnty, pixelColorArr3);
+// drawPixels(ctx,293,62, 60, 60, pixelQnty, pixelColorArr4);
+//
+// drawPixels(ctx,200,93, 60, 60, pixelQnty, pixelColorArr1);
+// drawPixels(ctx,231,93, 60, 60, pixelQnty, pixelColorArr2);
+// drawPixels(ctx,262,93, 60, 60, pixelQnty, pixelColorArr3);
+// drawPixels(ctx,293,93, 60, 60, pixelQnty, pixelColorArr4);
 
-drawPixels(ctx,200,0, 30, 30, pixelQnty, pixelColorArr1);
-drawPixels(ctx,231,0, 30, 30, pixelQnty, pixelColorArr2);
-drawPixels(ctx,262,0, 30, 30, pixelQnty, pixelColorArr3);
-
-drawPixels(ctx,200,31, 30, 30, pixelQnty, pixelColorArr1);
-drawPixels(ctx,231,31, 30, 30, pixelQnty, pixelColorArr2);
-drawPixels(ctx,262,31, 30, 30, pixelQnty, pixelColorArr3);
-
-drawPixels(ctx,200,62, 30, 30, pixelQnty, pixelColorArr1);
-drawPixels(ctx,231,62, 30, 30, pixelQnty, pixelColorArr2);
-drawPixels(ctx,262,62, 30, 30, pixelQnty, pixelColorArr3);
-
+}
 // теперь надо получить цвета с канваса.
 // делим изображение на квадраты и в каждом считаем средний цвет
 // добавляем его в массив
 // по этому массиву запускаем drawPixels
+
+let squareSize = 10;
+let imageSize = 100;
+let squaresNum = imageSize / squareSize;
+let resultColors = [];
+
+for(let i = 0; i < squaresNum; i++){ // перебираем квадраты
+    let sumColor = [0,0,0];
+    for(let j = i * squareSize; j < squareSize; j++){ // пиксели по высоте
+        for(let k = i * squareSize; k < squareSize; k++){ // пиксели по ширине
+            let pixelColorArr = ctx.getImageData(j,k,1,1).data;
+
+            console.log('j,k: ', j,k);
+            console.log('squareSize: ', squareSize);
+            console.log('pixelColorArr: ', pixelColorArr);
+
+            sumColor[0] = sumColor[0] + pixelColorArr[0];
+            sumColor[1] = sumColor[1] + pixelColorArr[1];
+            sumColor[2] = sumColor[2] + pixelColorArr[2];
+        }
+    }
+    resultColors.push([
+        (sumColor[0])/(squareSize*squareSize),
+        (sumColor[1])/(squareSize*squareSize),
+        (sumColor[2])/(squareSize*squareSize),
+    ])
+};
+
+console.log('result colors: ',resultColors)
+
