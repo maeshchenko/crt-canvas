91049ef69cfa939673e3fa8ce12ae6fd27ff7ee5
Mikhail Eshchenko <mikhail.eshchenko@gmail.com>
2024-01-31 15:30:46 +0800

    finish with video

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,57 +1,22 @@
-export function drawCRTPixel (ctx:CanvasRenderingContext2D,colorsArr: Uint8ClampedArray, xCoord: number, yCoord: number, pixelWidth:number, pixelHeight:number){
-    const colorWidth = pixelWidth / 100 * 32;
-    const spaceWidth = pixelWidth / 100 * 3;
-    let curX = xCoord;
-    let curY = yCoord;
-
-    // а что если делать не тусклость, а прозрачность?
-    // 255 - это 100%, 0 - это 0%
-    // 25 - это 10%
-    // 25.5/255*100 = 10
-    // Прозрачность будет нужна 0.1 -> значит на 100 умножать не надо
+export function drawCRTPixel (ctx:CanvasRenderingContext2D,colorsArr: number[] | Uint8ClampedArray, yCoord: number, xCoord: number, offsetY:number, offsetX: number){
+    const colorWidth = 1.8;
+    const spaceWidth = 0.2;
+    const spaceHeight = 0.2;
+    const pixelHeight = (colorWidth*3 + spaceWidth*2 + spaceHeight);
+    const pixelWidth = (colorWidth*3 + spaceWidth*3);
 
     ctx.beginPath();
-    ctx.roundRect(curX, curY, colorWidth,pixelHeight, 5 )
+    ctx.roundRect(xCoord*pixelWidth + offsetX, yCoord*pixelHeight + spaceHeight*yCoord + offsetY, colorWidth,pixelHeight, 5 )
     ctx.fillStyle = `rgba(255,0,0,${colorsArr[0]/255})`;
-    curX = curX + colorWidth + spaceWidth;
     ctx.fill();
 
     ctx.beginPath();
     ctx.fillStyle = `rgba(0,255,0,${colorsArr[1]/255})`;
-    ctx.roundRect(curX, curY, colorWidth,pixelHeight, 5 )
-    curX = curX + colorWidth + spaceWidth;
+    ctx.roundRect(xCoord*pixelWidth + spaceWidth + colorWidth + offsetX, yCoord*pixelHeight + spaceHeight*yCoord + offsetY, colorWidth,pixelHeight, 5 )
     ctx.fill();
 
     ctx.beginPath();
     ctx.fillStyle = `rgba(0,0,255,${colorsArr[2]/255})`;
-    ctx.roundRect(curX, curY, colorWidth,pixelHeight, 5 )
+    ctx.roundRect(xCoord*pixelWidth + spaceWidth*2 + colorWidth*2 + offsetX, yCoord*pixelHeight + spaceHeight*yCoord + offsetY, colorWidth,pixelHeight, 5 )
     ctx.fill();
 }
-
-export function drawPixels(ctx:CanvasRenderingContext2D,startX:number, startY:number, width:number, height:number, pixelsQuantity:number, colorsArr:Uint8ClampedArray){
-    const clusterWidth = width / pixelsQuantity;
-    const clusterHeight = height / pixelsQuantity;
-
-    const pixelWidth = clusterWidth / 100 * 98;
-    const spaceWidth = clusterWidth / 100 * 2;
-
-    const pixelHeight = clusterHeight / 100 * 98;
-    const spaceHeight = clusterHeight / 100 * 2;
-
-    let curX = startX;
-    let curY = startY;
-
-    for(let i = 0; i<pixelsQuantity; i++){
-        for(let j = 0; j<pixelsQuantity; j++){
-            if(j % 2 === 0 ){
-                curY = curY + clusterHeight / 2;
-            } else {
-                curY = curY - clusterHeight / 2;
-            }
-            drawCRTPixel(ctx,colorsArr, curX, curY, pixelWidth, pixelHeight);
-            curX = (curX + pixelWidth + spaceWidth) ;
-        }
-        curX = startX;
-        curY = (curY + pixelHeight + spaceHeight);
-    }
-};
--- src/index.ts
+++ src/index.ts
@@ -1,102 +1,52 @@
-import {drawPixels, drawCRTPixel} from './helpers'
+import {drawCRTPixel} from './helpers'
 
-const canvas = document.getElementsByTagName("canvas")[0];
-const ctx = canvas.getContext('2d');
+const canvas_source = document.getElementById("source") as HTMLCanvasElement;
+const ctx_source = canvas_source.getContext('2d');
 
-const pixelColors = ['#47bffe','#f0cfcf','#88ff1f','#93d4a5'];
+const canvas_result = document.getElementById('result-crt') as HTMLCanvasElement;
+const ctx_result = canvas_result.getContext('2d');
 
-ctx.fillStyle = '#4f534d';
-ctx.fillRect(0,0,canvas.width, canvas.height);
-ctx.fill();
-
-ctx.beginPath();
-ctx.roundRect(0, 0, 100,100, 0 )
-ctx.fillStyle = pixelColors[0];
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.roundRect(0, 110, 100,100, 0 )
-ctx.fillStyle = pixelColors[1];
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.roundRect(0, 220, 100,100, 0 )
-ctx.fillStyle = pixelColors[2];
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.roundRect(0, 330, 100,100, 0 )
-ctx.fillStyle = pixelColors[3];
-ctx.fill();
-ctx.closePath();
+ctx_source.fillStyle = '#4f534d';
+ctx_source.fillRect(0,0,canvas_result.width, canvas_result.height);
+ctx_source.fill();
 
 const img1 = new Image();
-img1.src = 'download.png';
-img1.onload = function (){
-    // ctx.drawImage(img1, 0,0, 100,100)
+img1.src = 'tv.png';
 
-let pixelColorArr1 = ctx.getImageData(10,10,1,1).data;
-let pixelColorArr2 = ctx.getImageData(10,110,1,1).data;
-let pixelColorArr3 = ctx.getImageData(10,220,1,1).data;
-let pixelColorArr4= ctx.getImageData(10,330,1,1).data;
-
-const pixelQnty = 20;
-
-drawPixels(ctx,200,0, 100, 100, pixelQnty, pixelColorArr1);
-drawPixels(ctx,200,110, 100, 100, pixelQnty, pixelColorArr2);
-drawPixels(ctx,200,220, 100, 100, pixelQnty, pixelColorArr3);
-drawPixels(ctx,200,330, 100, 100, pixelQnty, pixelColorArr4);
-
-// drawPixels(ctx,200,31, 60, 60, pixelQnty, pixelColorArr1);
-// drawPixels(ctx,231,31, 60, 60, pixelQnty, pixelColorArr2);
-// drawPixels(ctx,262,31, 60, 60, pixelQnty, pixelColorArr3);
-// drawPixels(ctx,293,31, 60, 60, pixelQnty, pixelColorArr4);
-//
-// drawPixels(ctx,200,62, 60, 60, pixelQnty, pixelColorArr1);
-// drawPixels(ctx,231,62, 60, 60, pixelQnty, pixelColorArr2);
-// drawPixels(ctx,262,62, 60, 60, pixelQnty, pixelColorArr3);
-// drawPixels(ctx,293,62, 60, 60, pixelQnty, pixelColorArr4);
-//
-// drawPixels(ctx,200,93, 60, 60, pixelQnty, pixelColorArr1);
-// drawPixels(ctx,231,93, 60, 60, pixelQnty, pixelColorArr2);
-// drawPixels(ctx,262,93, 60, 60, pixelQnty, pixelColorArr3);
-// drawPixels(ctx,293,93, 60, 60, pixelQnty, pixelColorArr4);
-
+img1.onload = function(){
+    ctx_result.drawImage(img1, 0, 0, canvas_result.width, canvas_result.height);
 }
-// теперь надо получить цвета с канваса.
-// делим изображение на квадраты и в каждом считаем средний цвет
-// добавляем его в массив
-// по этому массиву запускаем drawPixels
 
-let squareSize = 10;
-let imageSize = 100;
-let squaresNum = imageSize / squareSize;
-let resultColors = [];
+const video = document.getElementById("source-video") as HTMLVideoElement;
+const video_btn = document.getElementById('btn') as HTMLButtonElement;
 
-for(let i = 0; i < squaresNum; i++){ // перебираем квадраты
-    let sumColor = [0,0,0];
-    for(let j = i * squareSize; j < squareSize; j++){ // пиксели по высоте
-        for(let k = i * squareSize; k < squareSize; k++){ // пиксели по ширине
-            let pixelColorArr = ctx.getImageData(j,k,1,1).data;
-
-            console.log('j,k: ', j,k);
-            console.log('squareSize: ', squareSize);
-            console.log('pixelColorArr: ', pixelColorArr);
-
-            sumColor[0] = sumColor[0] + pixelColorArr[0];
-            sumColor[1] = sumColor[1] + pixelColorArr[1];
-            sumColor[2] = sumColor[2] + pixelColorArr[2];
-        }
+video_btn.onclick = () => {
+    if(!video.paused){
+        video.pause();
+    } else {
+       video.play();
     }
-    resultColors.push([
-        (sumColor[0])/(squareSize*squareSize),
-        (sumColor[1])/(squareSize*squareSize),
-        (sumColor[2])/(squareSize*squareSize),
-    ])
-};
+}
 
-console.log('result colors: ',resultColors)
+const x_video_offset = 220;
+const y_video_offset = 284;
 
+video.addEventListener("play", () => {
+    function step() {
+        ctx_result.fillStyle = '#2a2a2a';
+        ctx_result.fillRect(110,280,592, 384);
+        ctx_result.fill();
+
+        ctx_source.drawImage(video, 0, 0, 60, 60);
+
+        for(let h = 0; h<canvas_source.height; h++){
+            for(let w = 0; w<canvas_source.width; w++){
+                let arr = ctx_source.getImageData(w,h,1,1).data;
+                drawCRTPixel(ctx_result,arr,h,w,y_video_offset,x_video_offset)
+            }
+        }
+
+        requestAnimationFrame(step);
+    }
+    requestAnimationFrame(step);
+});
